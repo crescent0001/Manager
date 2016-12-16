fb43eb20f0bccdf65cba155087f40409362ad5bb
hehefei <hefei.he95@gmail.com>
2016-12-16 08:48:59 +0800

    upload connector and sqlAccess

---
+++ src/database/SqlAccess.java
@@ -0,0 +1,83 @@
+package database;
+
+import java.sql.*;
+
+/**
+ * Created by hehef on 12/15/2016.
+ */
+public class SqlAccess {
+    private final String DRIVER="com.mysql.jdbc.Driver";
+    private String databaseURL="jdbc:mysql://localhost:3306/manager?useSSL=false";
+    private Connection connection=null;
+    private Statement statement=null;
+    private ResultSet rs;
+
+    private String userName;
+    private String password;
+
+
+    public SqlAccess(){
+        //register driver
+        userName="user";
+        password="12345";
+
+        try{
+            Class.forName(DRIVER);
+        }catch(ClassNotFoundException e){
+            System.out.println("Driver not fount :"+e.getMessage());
+        }
+    }
+    public void openConnection(){
+        try{
+            connection= DriverManager.getConnection(databaseURL,userName,password);
+            statement=connection.createStatement();
+        }catch (SQLException e){
+            System.out.println("Connection fail "+e.getMessage()+" either wrong password or username");
+        }
+    }
+    public void insertData(String tableName,String ...values){
+
+        if(statement!=null){
+            try{
+                for(String v : values){
+                    System.out.println("INSERT INTO "+tableName+" VALUES("+v+")");
+                    statement.executeUpdate("INSERT INTO "+tableName+" VALUES("+v+")");
+                }
+            }catch (SQLException e){
+                System.out.println(e.getMessage());
+            }
+
+
+        }
+        else{
+            System.out.println("Open connection first");
+        }
+
+
+    }
+    public ResultSet retriveData(String query){
+
+        try {
+            rs = statement.executeQuery(query);
+
+        }catch (SQLException e){
+            System.out.println("Error:"+e.getMessage());
+        }
+        return rs;
+    }
+    public void closeConnection(){
+        if(rs!=null){
+            try{
+                rs.close();
+            } catch (Exception e){ /*do nothing*/}
+        }
+        if(statement!=null){
+            try{
+                rs.close();
+            }catch (Exception e){/*do nothing*/}
+        }
+
+
+    }
+
+}
--- src/main/scene/CreateAccountController.java
+++ src/main/scene/CreateAccountController.java
@@ -3,16 +3,17 @@ package main.scene;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
 
+import database.SqlAccess;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import javafx.scene.Node;
 
@@ -22,17 +23,63 @@ public class CreateAccountController implements Initializable {
 
     @FXML
     private ComboBox<String> month;
+    @FXML
+    private TextField yearField;
 
     @FXML
-    private ToggleGroup gender;
+    private TextField adminNoField;
+
+    @FXML
+    private ToggleGroup genderGroup;
+
+    @FXML
+    private TextField dayField;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private RadioButton radioButMale;
+
+    @FXML
+    private RadioButton radioButOther;
+
+    @FXML
+    private RadioButton radioButFemale;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
         month.getItems().addAll("January","Febuary","March","April","May","June","July","August","September","October","November","December");
+        radioButFemale.setUserData("Female");
+        radioButMale.setUserData("Male");
+        radioButOther.setUserData("Other");
 
 
     }
     @FXML
     void signUp(ActionEvent event) {
+        String name=nameField.getText();
+        String gender=genderGroup.getSelectedToggle().getUserData().toString();
+        //Date birthDate=new Date();
+
+        String adminNo=adminNoField.getText();
+        String email=emailField.getText();
+        String password=passwordField.getText();
+        SqlAccess sqlAccess=new SqlAccess();
+        sqlAccess.openConnection();
+        sqlAccess.insertData("user","\""+name+"\",\""+gender+"\","+"3/25/2016"+",\""+adminNo+"\",\""+email+"\",\""+password+"\"");
+        sqlAccess.closeConnection();
+
+
+
+
         try{
             Stage stage=(Stage)((Node)event.getTarget()).getScene().getWindow();
             Parent parent= FXMLLoader.load(getClass().getResource("Login.fxml"));
